104c0bd015705e1c5424f35c4484aff740d764f8
Dimitar Lilov <dimitar.lilov.mici@gmail.com>
2019-05-15 19:55:26 +0300

    Added ConsoleReader and ConsoleRender

---
+++ Hangman/Hangman/Contracts/IRenderer.cs
@@ -0,0 +1,21 @@
+﻿namespace Hangman.Contracts
+{
+
+    /// <summary>
+    /// Abstraction for rendering on the UI
+    /// </summary>
+    public interface IRenderer
+    {
+
+        /// <summary>
+        /// Method for writing the text output
+        /// </summary>
+        void WriteLine(string message, params object[] param);
+
+
+        /// <summary>
+        /// Method for clear the text output
+        /// </summary>
+        void Clear();
+    }
+}
---
+++ Hangman/Hangman/UI/ConsoleRender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman.UI
+{
+    class ConsoleRender
+    {
+    }
+}
